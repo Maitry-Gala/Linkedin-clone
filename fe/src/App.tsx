c565323f08ee806bb65a0e6c4b784096d6f9195d
Maitry Gala <maitryg1004@gmail.com>
2026-05-26 23:03:39 +0530

    Init fe

---
+++ fe/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import TopBar from './components/Topbar'
+
+function App() {
+
+  return (
+    <>
+      <TopBar />
+    </>
+  )
+}
+
+export default App
---
+++ fe/src/components/Topbar.tsx
@@ -0,0 +1,12 @@
+export default function TopBar() {
+    return(
+        <div className="flex justify-between">
+            <div>
+                child one
+            </div>
+            <div>
+                child two
+            </div>
+        </div>
+    )
+}
